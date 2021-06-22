2b0e60e81f84a4ae7bae03823e062f207f0d1e58
ejpv <erickjpv@hotmail.es>
2021-06-21 20:59:06 -0500

    cambio en resources para que agarre la ip dle back

--- src/plugins/resources.js
+++ src/plugins/resources.js
@@ -3,6 +3,7 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
 
+Vue.http.options.root = process.env.PROXY
+
 const token = sessionStorage.getItem('token')
-// eslint-disable-next-line
 Vue.http.headers.common['token'] = token
